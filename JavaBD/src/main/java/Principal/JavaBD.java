08acdff5b0f5d946a8dbb0265612f9a17f5873ce
rudybravoumg <rbravod@miumg.edu.gt>
2022-09-15 11:30:05 -0600

    Update JavaBD.java

--- JavaBD/src/main/java/Principal/JavaBD.java
+++ JavaBD/src/main/java/Principal/JavaBD.java
@@ -6,7 +6,12 @@ package Principal;
  */
 public class JavaBD {
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Linea 2 de Bienvenida");
     }
 }
